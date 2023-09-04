413923e03598c4c915ec2c47bbab9596ddfeaa39
dtzz81 <denisa.turkova@madetech.com>
2023-09-04 14:21:54 +0100

    refactoring, adjusting bad test and removing codes

--- probability_calculator/src/calc.py
+++ probability_calculator/src/calc.py
@@ -13,41 +13,39 @@ class Hat:
         self.contents = self.ball_list
 
     def draw(self, number_of_draw_balls):
-        if number_of_draw_balls >= len(self.contents):
-            drawn_balls = self.contents
-            self.contents = []
-        else:
-            drawn_balls = random.sample(self.contents, number_of_draw_balls)
-            for ball in drawn_balls:
-                self.contents.remove(ball)
-        return drawn_balls
-
-    def probability (self, ball_colour):
-        balls_total = len(self.contents)
-        colours_total = self.contents.count(ball_colour)
-        return colours_total / balls_total
+        if number_of_draw_balls > len(self.contents):
+            return self.contents.copy()
+        # number_of_draw_balls is always the minimum between number_of_draw_balls and len(self.contents)
+        return random.sample(self.contents, number_of_draw_balls)
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    expected_balls_matching_result = 0
+    successful_experiment = 0
 
-    for i in range(num_experiments):
-        new_hat = copy.deepcopy(hat)
-        drawn_balls = new_hat.draw(num_balls_drawn)
+    for _ in range(num_experiments):
+        drawn_balls = hat.draw(num_balls_drawn)
         counts = {}
+
+        # transform drawn balls list into a dictionary
         for ball in drawn_balls:
+            # if ball in counts.keys():
+            #     counts[ball] += 1
+            # else:
+            #     counts[ball] = 1
             counts[ball] = counts.get(ball, 0) + 1
 
+        # are all drawn balls into expected? (same number)
         compare = all(expected_balls.get(color, 0) <= counts.get(color, 0)
                         for color in expected_balls.keys())
 
+        # compare = True
+        # for color in expected_balls.keys():
+        #     compare = compare and expected_balls.get(color, 0) <= counts.get(color, 0)
+
         if compare:
-            expected_balls_matching_result += 1
+            successful_experiment += 1
 
-    probability = expected_balls_matching_result / num_experiments
+    probability = successful_experiment / num_experiments
     return probability
-
-
-
 
 # inilise result, compute the result and return result
 #output   list = ("black", "red", "blue")
--- probability_calculator/tests/calc_test.py
+++ probability_calculator/tests/calc_test.py
@@ -21,24 +21,18 @@ def test_hat_class_contents_their_test():
 
 def test_probability_is_fifty_fifty():
     hat = Hat(blue=1, green=1)
-    probability_green = hat.probability("green")
+    probability_green = experiment(hat, {"green": 1}, 1, 10000)
     actual = probability_green
     expected = 0.5
-    assert actual == expected
+    assert (actual - expected) < 0.01
 
 def test_draw_number_exceeds_original_number_of_balls():
     hat = Hat(blue=1, green=1)
     hat.draw(3)
-    if "green" in hat.contents:
-        probability_green = hat.probability("green")
-        actual = probability_green
-        expected = 0.5
-        assert actual == expected
-    else:
-        assert len(hat.contents) == 0
+    assert len(hat.contents) == 2
 
 def test_probability_count():
-    probability = experiment(Hat(blue=3, red=2, green=6), expected_balls={"blue": 2, "green": 1}, num_balls_drawn=4, num_experiments=1000)
+    probability = experiment(Hat(blue=3, red=2, green=6), expected_balls={"blue": 2, "green": 1}, num_balls_drawn=4, num_experiments=10000)
     actual = probability
     print(actual)
     expected = 0.272
